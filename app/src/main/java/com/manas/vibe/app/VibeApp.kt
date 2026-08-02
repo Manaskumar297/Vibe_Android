e019699557538c2c5380a9394f62833cfd1d330a
manasKumarr <manas.jena@agicent.in>
2026-08-03 00:56:13 +0530

    navigation

--- app/src/main/java/com/manas/vibe/app/VibeApp.kt
+++ app/src/main/java/com/manas/vibe/app/VibeApp.kt
@@ -1,8 +1,9 @@
 package com.manas.vibe.app
 
 import androidx.compose.runtime.Composable
+import com.manas.vibe.app.navigation.AppNavigation
 
 @Composable
 fun VibeApp() {
-
+    AppNavigation()
 }
--- app/src/main/java/com/manas/vibe/app/navigation/AppNavigation.kt
+++ app/src/main/java/com/manas/vibe/app/navigation/AppNavigation.kt
@@ -1,2 +1,16 @@
 package com.manas.vibe.app.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation(){
+    val navController= rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ){
+
+    }
+}
